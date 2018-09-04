5d7ebf428d483c51b462aab746617303f4fe4ad1
Felix Sebastian <hello@felix-sebastian.com>
2018-09-04 14:56:17 +1000

    kicked off front end with basic layout

--- front-end/swin-pharma/src/index.js
+++ front-end/swin-pharma/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App.jsx';
+import registerServiceWorker from './scripts/registerServiceWorker';
+import { Provider } from 'react-redux'
+import getStore from './scripts/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={getStore()}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
