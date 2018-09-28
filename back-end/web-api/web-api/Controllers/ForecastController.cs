fe637971964a4be037496d8a0b91f1d77a11d18c
Xavier S <xavier.slattery@gmail.com>
2018-09-28 23:51:23 +1000

    Monthly Forecast Work

--- back-end/web-api/web-api/Controllers/ForecastController.cs
+++ back-end/web-api/web-api/Controllers/ForecastController.cs
@@ -140,5 +140,113 @@ namespace web_api.Controllers
 
             return StatusCode(400);
         }
+
+        // Generateing the monthly forecast:
+        [HttpGet]
+        [Route("Month/")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        public IActionResult GetMonth([FromQuery(Name = "todayDate")] string today, [FromQuery(Name = "date")] string date)
+        {
+            System.Diagnostics.Debug.WriteLine("########## FORECAST MONTH GET Inventory");
+            Console.WriteLine("########## FORECAST MONTH GET Inventory");
+
+            DateTime startDate;
+            DateTime todayDate;
+            if (DateTime.TryParse(date, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out startDate) && DateTime.TryParse(today, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out todayDate))
+            {
+                if (!InventoryController.itemTableLoadedFromFile)
+                {
+                    InventoryController c = new InventoryController();
+                }
+
+                if (!SalesController.salesTableLoadedFromFile)
+                {
+                    SalesController sc = new SalesController();
+                }
+
+                int dayOffset = (int)startDate.Day;
+                startDate = startDate.AddDays(-dayOffset);
+
+                string result = "{";
+                //result += "\"row\": [";
+
+                //InventoryController.itemTableLock.WaitOne();
+                //foreach (var item in InventoryController.itemTable)
+                //{
+                //result += "{";
+                //result += "\"name\":\"" + item.name + "\",";
+
+                //result += "\"day\": [ ";
+                //for (int i = 0; i < dayLength; i++)
+                //{
+                //    double proffit = 0;
+                //    DateTime currentDate = startDate.AddDays(i);
+
+                //    SalesController.salesTableLock.WaitOne();
+                //    foreach (var sale in SalesController.salesTable)
+                //    {
+                //        if (item.id == sale.itemID)
+                //        {
+                //            DateTime compareDate;
+                //            if (DateTime.TryParse(sale.date, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out compareDate))
+                //            {
+                //                if (compareDate == currentDate)
+                //                {
+                //                    proffit += Double.Parse(item.purchasePrice) * sale.quantity;
+                //                }
+                //            }
+                //        }
+                //    }
+                //    SalesController.salesTableLock.ReleaseMutex();
+
+                //    result += proffit + ",";
+                //}
+                //result = result.Remove(result.Length - 1);
+                //result += "],";
+
+                //    result += "\"forecast\": [ ";
+                //    for (int i = dayLength; i < dayLength + forecastLength; i++)
+                //    {
+                //        double rollingAverage = 0;
+                //        double averageCounter = 0;
+                //        DateTime currentDate = startDate.AddDays(i);
+
+                //        SalesController.salesTableLock.WaitOne();
+                //        foreach (var sale in SalesController.salesTable)
+                //        {
+                //            if (item.id == sale.itemID)
+                //            {
+                //                DateTime compareDate;
+                //                if (DateTime.TryParse(sale.date, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out compareDate))
+                //                {
+                //                    if (compareDate.DayOfWeek == currentDate.DayOfWeek)
+                //                    {
+                //                        rollingAverage += Double.Parse(item.purchasePrice) * sale.quantity;
+                //                        averageCounter++;
+                //                    }
+                //                }
+                //            }
+                //        }
+                //        SalesController.salesTableLock.ReleaseMutex();
+
+                //        result += (rollingAverage / averageCounter) + ",";
+                //    }
+                //    result = result.Remove(result.Length - 1);
+                //    result += "]";
+                //    result += "},";
+                //}
+                //InventoryController.itemTableLock.ReleaseMutex();
+                //result = result.Remove(result.Length - 1);
+
+                //result += "]";
+                result += "}";
+
+                return Ok(result);
+            }
+
+            return StatusCode(400);
+        }
+
     }
 }
+
