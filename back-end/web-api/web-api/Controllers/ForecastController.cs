d235a79fa3cf5173c66614b4b98e84c64d1d8d26
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2018-10-02 21:14:24 +1000

    Forcast Groupings

--- back-end/web-api/web-api/Controllers/ForecastController.cs
+++ back-end/web-api/web-api/Controllers/ForecastController.cs
@@ -12,14 +12,15 @@ namespace web_api.Controllers
     [Route("api/[controller]")]
     public class ForecastController : Controller
     {
-        // Generateing the weekly forecast:
+        // Generateing the weekly forecast, single item:
         [HttpGet]
-        [Route("Week/")]
+        [Route("Individual/Week/")]
         [ProducesResponseType(200, Type = typeof(string))]
-        public IActionResult GetWeek([FromQuery(Name = "todayDate")] string today, [FromQuery(Name = "date")] string date)
+        public IActionResult GetWeekSingle
+            ([FromQuery(Name = "todayDate")] string today, [FromQuery(Name = "date")] string date)
         {
-            System.Diagnostics.Debug.WriteLine("########## FORECAST WEEK GET Inventory");
-            Console.WriteLine("########## FORECAST WEEK GET Inventory");
+            System.Diagnostics.Debug.WriteLine("########## FORECAST GetWeekSingle GET Inventory");
+            Console.WriteLine("########## FORECAST GetWeekSingle GET Inventory");
 
             DateTime startDate;
             DateTime todayDate;
@@ -141,9 +142,139 @@ namespace web_api.Controllers
             return StatusCode(400);
         }
 
-        // Generateing the monthly forecast:
+        // Generating the weekly forcast, group of items:
         [HttpGet]
-        [Route("Month/")]
+        [Route("Group/Week/")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        public IActionResult GetWeekGroup([FromQuery(Name = "todayDate")] string today, [FromQuery(Name = "date")] string date)
+        {
+            System.Diagnostics.Debug.WriteLine("########## FORECAST GetWeekGroup GET Inventory");
+            Console.WriteLine("########## FORECAST GetWeekGroup GET Inventory");
+
+            DateTime startDate;
+            DateTime todayDate;
+            if (DateTime.TryParse(date, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out startDate) && DateTime.TryParse(today, new CultureInfo("en-AU"), System.Globalization.DateTimeStyles.AssumeLocal, out todayDate))
+            {
+                // Initiallising Required Controller
+                if (!InventoryController.itemTableLoadedFromFile)
+                {
+                    InventoryController c = new InventoryController();
+                }
+
+                if (!SalesController.salesTableLoadedFromFile)
+                {
+                    SalesController sc = new SalesController();
+                }
+
+                int dayOffset = (int)startDate.DayOfWeek;
+                startDate = startDate.AddDays(-dayOffset);
+
+                TimeSpan differenceDate = todayDate - startDate;
+                int difference = differenceDate.Days;
+                int dayLength = difference + 1;
+                int forecastLength = 7 - difference - 1;
+                if (difference >= 7)
+                {
+                    dayLength = 7;
+                    forecastLength = 0;
+                }
+                else if (difference == 0)
+                {
+                    dayLength = 1;
+                    forecastLength = 6;
+                }
+                else if (difference < 0)
+                {
+                    dayLength = 0;
+                    forecastLength = 7;
+                }
+
+                if (dayLength > 7) dayLength = 7;
+                if (forecastLength > 7) forecastLength = 7;
+
+                string result = "{";
+                result += "\"row\": [";
+
+                InventoryController.itemTableLock.WaitOne();
+                foreach (var item in InventoryController.itemTable)
+                {
+                    result += "{";
+                    result += "\"name\":\"" + item.name + "\",";
+
+                    result += "\"day\": [ ";
+                    for (int i = 0; i < dayLength; i++)
+                    {
+                        double proffit = 0;
+                        DateTime currentDate = startDate.AddDays(i);
+
+                        SalesController.salesTableLock.WaitOne();
+                        foreach (var sale in SalesController.salesTable)
+                        {
+                            if (item.id == sale.itemID)
+                            {
+                                DateTime compareDate;
+                                if (DateTime.TryParse(sale.date, new CultureInfo("en-AU"), DateTimeStyles.AssumeLocal, out compareDate))
+                                {
+                                    if (compareDate == currentDate)
+                                    {
+                                        proffit += Double.Parse(item.purchasePrice) * sale.quantity;
+                                    }
+                                }
+                            }
+                        }
+                        SalesController.salesTableLock.ReleaseMutex();
+
+                        result += proffit + ",";
+                    }
+                    result = result.Remove(result.Length - 1);
+                    result += "],";
+
+                    result += "\"forecast\": [ ";
+                    for (int i = dayLength; i < dayLength + forecastLength; i++)
+                    {
+                        double rollingAverage = 0;
+                        double averageCounter = 0;
+                        DateTime currentDate = startDate.AddDays(i);
+
+                        SalesController.salesTableLock.WaitOne();
+                        foreach (var sale in SalesController.salesTable)
+                        {
+                            if (item.id == sale.itemID)
+                            {
+                                DateTime compareDate;
+                                if (DateTime.TryParse(sale.date, new CultureInfo("en-AU"), DateTimeStyles.AssumeLocal, out compareDate))
+                                {
+                                    if (compareDate.DayOfWeek == currentDate.DayOfWeek)
+                                    {
+                                        rollingAverage += Double.Parse(item.purchasePrice) * sale.quantity;
+                                        averageCounter++;
+                                    }
+                                }
+                            }
+                        }
+                        SalesController.salesTableLock.ReleaseMutex();
+
+                        result += (rollingAverage / averageCounter) + ",";
+                    }
+                    result = result.Remove(result.Length - 1);
+                    result += "]";
+                    result += "},";
+                }
+                InventoryController.itemTableLock.ReleaseMutex();
+                result = result.Remove(result.Length - 1);
+
+                result += "]";
+                result += "}";
+
+                return Ok(result);
+            }
+
+            return StatusCode(400);
+        }
+
+        // Generateing the monthly forecast, single item:
+        [HttpGet]
+        [Route("Individual/Month/")]
         [ProducesResponseType(200, Type = typeof(string))]
         public IActionResult GetMonth([FromQuery(Name = "todayDate")] string today, [FromQuery(Name = "date")] string date)
         {
